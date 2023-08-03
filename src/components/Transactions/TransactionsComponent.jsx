4cff36a22dbb904a9a79e81e3937de8922e0359c
Matias Cancina <cancinamatias@gmail.com>
2023-08-03 15:54:22 -0300

    Feat(cleint/pagination): crea paginado

---
+++ src/components/Transactions/TransactionsComponent.jsx
@@ -0,0 +1,30 @@
+'use client'
+import usePagination from '@/customHooks/usePagination'
+import React from 'react'
+import PaginationComponent from '../Pagination/PaginationComponent';
+import styles from "../Dashboard/dashboard.module.css";
+
+const TransactionsComponent = () => {
+    const {transactions, count, totalPages, nextHandler, prevHandler, firstPageHandler, lastPageHandler} = usePagination() 
+
+  return (
+        <div className={`${styles.mySpendings}  ${styles.shadow_background} w-[1059px] h-[380px]`}>
+            {transactions.length && transactions.map((t, index) => (
+                <p key={index}>{t.name}</p>
+            ))}
+            
+            {transactions.length && 
+                <PaginationComponent
+                  count={count}
+                  totalPages={totalPages}
+                  nextHandler={nextHandler}
+                  prevHandler={prevHandler}
+                  firstPageHandler={firstPageHandler}
+                  lastPageHandler={lastPageHandler}
+                />
+              }
+        </div>
+  )
+}
+
+export default TransactionsComponent
