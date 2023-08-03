2d6dc4901f61d573bce4dada26e92d5879750b90
Victoria Rodríguez <vickyrodriguez544@gmail.com>
2023-08-03 12:29:12 -0300

    paso el side right a un componente llamado SideRight

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Logo from '@/utils/Images/logoFull.png';
-import Image from 'next/image';
+import SideRight from "../SideRight/SideRight"; 
+
 
 export default function SignUpComponent () {
     return (
@@ -22,14 +22,7 @@ export default function SignUpComponent () {
                 <h3 className="text-gray-500 mt-5">Already have an account?</h3>
                 <h4 className="text-regularPink font-medium">Login</h4>
             </div>
-            <div className="w-2/4 bg-gradient-to-r from-boldPink to-regularPink flex items-center  justify-center ">
-                <div className="bg-white rounded-3xl h-3/4 w-3/5 relative  backdrop-opacity-10 backdrop-invert bg-white/30">
-                    <div className="relative top-20">
-                        <h1 className="text-white font-bold text-center text-5xl">Welcome to</h1>
-                        <Image className="h-fit relative top-1/4 " src={Logo} />
-                    </div>
-                </div>
-            </div>
+           <SideRight/>
         </div>
     )
 }; 
