3893aa54a4c7b86ba63e86b315a2e6f7e5fce78b
laurenciopaez <laurencio314@gmail.com>
2023-08-03 14:28:31 -0300

    toma pa vo

--- src/components/Wallet/WalletComponent.jsx
+++ src/components/Wallet/WalletComponent.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import styles from './wallet.module.css'
 
 export default function WalletComponent () {
     return (
         <div>
-            wallet
+            <div className={styles.container}>
+                <div className={styles.grid_2x2}>
+                    <div className={`${styles.myBalance} ${styles.shadow_background}`}></div>
+                    <div  className={`${styles.form} ${styles.shadow_background}`}></div>
+                    <div  className={`${styles.spenByCategory} ${styles.shadow_background}`}></div>
+                </div>
+            </div>
         </div>
     )
 }
