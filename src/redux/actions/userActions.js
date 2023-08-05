fb8bec72d56efa2080ee22dfb09faf50bd0af984
RodrigoSpano <rorrospano17@gmail.com>
2023-08-05 17:50:31 -0300

    (client/alerts): handle errors and alerts)

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
 import axios from 'axios'
+import Swal from "sweetalert2"
 
 
 
@@ -21,12 +22,32 @@ export const loginUser = createAsyncThunk('user/login', async (userData) => {
     if (data.token) {
       const auth = await authorizeUser(data.token)
       if (auth.logged) {
-        alert('logeado perri')
+        Swal.fire({
+          icon: 'success',
+          title: 'Logged successfully',
+          showConfirmButton: false,
+          timer: 1000
+        })
         return { user: data.user, logged: true }
       }
     }
   } catch (error) {
-    alert('error')
     console.log(error)
+    if (error.response) {
+      Swal.fire({
+        icon: 'error',
+        title: error.response.data.message,
+        showConfirmButton: false,
+        timer: 1500
+      })
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: error.message,
+        text: 'Try again later!',
+        showConfirmButton: false,
+        timer: 2000
+      })
+    }
   }
 })
