fe21c259df5547dd36d323fe1a849317a1774a65
RodrigoSpano <rorrospano17@gmail.com>
2023-08-06 18:29:18 -0300

    fix(client/dashboard): correccion de estilos

--- src/components/Dashboard/DashboardComponent.jsx
+++ src/components/Dashboard/DashboardComponent.jsx
@@ -8,11 +8,9 @@ import { useSelector } from "react-redux";
 export default function DashboardComponent() {
   const user = useSelector((state) => state.user);
   return (
-    <>
-      <div>
-        <div className="container mx-auto">
-          <div className="grid grid-cols-1 sm:grid-cols-12 h-2/5">
-            <div className="col-span-5 flex flex-col p-3vh 0 0 2vh ml-6">
+        <div className="flex flex-col gap-5 pt-5 px-10">
+          <div className="grid grid-cols-1 sm:grid-cols-12 h-2/5 gap-5">
+            <div className="col-span-5 flex flex-col pl-6">
               <div className="font-bold text-4xl leading-normal text-slate-900 mt-10 ">
                 Welcome Back {user.name.replace(/^\w/, (c) => c.toUpperCase())}
               </div>
@@ -20,71 +18,69 @@ export default function DashboardComponent() {
                 Pig Commander is the most secure finance app.
               </div>
             </div>
-            <div className="flex flex-row bg-white rounded-lg shadow-md mt-10 col-span-4 ">
+            <div className="flex flex-row bg-white rounded-lg shadow-md col-span-4 gap-10 h-[120px] ">
               <div className="rounded-tl-2xl rounded-bl-2xl w-3 h-full bg-pink-400"></div>
-              <div className="flex flex-col p-13 pl-8">
-                <div className="text-slate-950  text-xl font-semibold pl-7vw">
+              <div className="flex flex-col justify-between py-2 w-full">
+                <p className="text-[#AFAFAF] text-base font-Poppins font-medium">
                   My Balance
-                </div>
-                <div className="text-regularPink text-4xl font-semibold pl-7vw">
+                </p>
+                <span className="text-strongPink text-5xl font-Poppins font-bold text-right pr-5 ">
                   $255.000,00
-                </div>
+                </span>
               </div>
             </div>
-            <div className="bg-white rounded-lg shadow-md m-5 row-span-3 col-span-3">
-              <div className="flex flex-col justify-center content-center">
-                <div className="w-120 h-120 rounded-full mx-auto bg-gray-950 flex items-center justify-center">
+              <div className="bg-white rounded-lg shadow-md row-span-3 col-span-3  flex flex-col justify-center items-center">
+                <div className="w-[120px] h-[120px] rounded-full mx-auto bg-gray-950 flex items-center justify-center">
                 </div>
-                <div className="text-24 font-bold">Nombre del Usuario</div>
+                <span className="text-24 font-bold">Nombre del Usuario</span>
               </div>
-            </div>
-            <div className=" bg-white rounded-lg shadow-md ml-5 mt-5 mb-5 flex flex-row col-span-3 row-span-2  ">
+            <div className=" bg-white rounded-lg shadow-md flex gap-10 flex-row col-span-3 row-span-2 h-[100px]">
               <div className="rounded-tl-2xl rounded-bl-2xl w-3 h-full bg-pink-400"></div>
-              <div className="flex flex-col p-13 pl-16">
-                <div className="text-slate-950  text-xl font-semibold pl-7vw">
+
+              <div className="flex flex-col justify-evenly ">
+              <p className="text-[#AFAFAF] text-base font-Poppins font-medium">
                   Expenses
-                </div>
-                <div className="text-gray-950 text-25 font-semibold pt-20%">
+                </p>
+                <span className="text-[#15223C] text-[20px] font-semibold">
                   $155.000
-                </div>
+                </span>
               </div>
             </div>
-            <div className=" bg-white rounded-lg shadow-md ml-5 mt-5 mb-5 flex flex-row col-span-3 row-span-2  ">
+            <div className=" bg-white rounded-lg shadow-md gap-10 flex flex-row col-span-3 row-span-2 h-[100px]">
               <div className="rounded-tl-2xl rounded-bl-2xl w-3 h-full bg-pink-400"></div>
-              <div className="flex flex-col p-13 pl-16">
-                <div className="text-slate-950 text-xl font-semibold pl-7vw">
+              <div className="flex flex-col justify-evenly">
+                <p className="text-[#AFAFAF] text-base font-Poppins font-medium">
                   Income
-                </div>
-                <div className="text-gray-950  text-25 font-semibold pt-20%">
+                </p>
+                <span className="text-[#15223C] text-[20px] font-semibold">
                   $75.00
-                </div>
+                </span>
               </div>
             </div>
-            <div className=" bg-white rounded-lg shadow-md ml-5 mt-5 mb-5 flex flex-row col-span-3 row-span-2  ">
+            
+            <div className=" bg-white rounded-lg shadow-md gap-10 flex flex-row col-span-3 row-span-2  h-[100px]">
               <div className="rounded-tl-2xl rounded-bl-2xl w-3 h-full bg-pink-400"></div>
-              <div className="flex flex-col p-13 pl-10">
-                <div className="text-gray-950 font-poppins text-xl font-semibold pl-7vw">
+              <div className="flex flex-col justify-evenly">
+                <p className="text-[#AFAFAF] text-base font-Poppins font-medium">
                   Savings
-                </div>
-                <div className="text-gray-950 font-poppins text-25 font-semibold pt-20%">
+                </p>
+                <span className="text-[#15223C] text-[20px] font-semibold">
                   $93.222
-                </div>
+                </span>
               </div>
             </div>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-3 h-3/5">
-            <div className="col-span-3 row-span-5 bg-white rounded-lg shadow-md ml-5 pb-24">
+          <div className="grid grid-cols-1 sm:grid-cols-3 h-3/5 gap-5">
+            <div className="col-span-3 row-span-5 bg-white rounded-lg shadow-md h-[330px]">
               myJournal
             </div>
-            <div className="bg-white rounded-lg shadow-md ml-5 mt-5 col-span-2 ">
+            <div className="bg-white rounded-lg shadow-md col-span-2 ">
             <TransactionsComponent />
             </div>
             
-            <div className=" bg-white rounded-lg shadow-md ml-5 mt-5">myGraph</div>
+            <div className=" bg-white rounded-lg shadow-md ">myGraph</div>
           </div>
         </div>
-      </div>
-    </>
   );
 }
