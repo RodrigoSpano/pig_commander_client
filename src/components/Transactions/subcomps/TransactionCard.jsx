2652c87121e26aa26cf127320a11bf807c6e6b98
lihuel <zeregalihuel@gmail.com>
2023-08-09 15:59:10 -0300

    paginado last transacctions

--- src/components/Transactions/subcomps/TransactionCard.jsx
+++ src/components/Transactions/subcomps/TransactionCard.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { BiTrendingUp, BiTrendingDown } from 'react-icons/bi';
 
-const TransactionCard = ({transaction, handelDetail}) => {
+const TransactionCard = ({ transaction, handelDetail }) => {
+  const containerVariants = {
+    hidden: { opacity: 0, y: 10 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
+  };
+
+  const cardHoverVariants = {
+    hover: { scale: 1.05 },
+  };
+
   return (
-    <div className='flex gap-80 h-[32px] cursor-pointer border-b-2 border-slate-200' onClick={() => {handelDetail(transaction)}}>
-         <p className='w-[120px] overflow-hidden' >{transaction.name}</p>
-          <div>
-            <p className={`font-bold ${transaction.type === 'expense' ? 'text-red-500' : 'text-green-500'}`}>
-              ${transaction.mount}
-              </p>
-          </div>
-        </div>
-  )
-}
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      className='flex items-center justify-between px-6 py-4 mx-16 my-2 bg-white border border-gray-200 rounded-lg cursor-pointer'
+      onClick={() => { handelDetail(transaction) }}
+    >
+      <div className='flex flex-col'>
+        <p className='text-lg font-semibold'>{transaction.name}</p>
+        <p className={`text-sm ${transaction.type === 'expense' ? 'text-red-500' : 'text-green-500'}`}>
+          $ {transaction.mount}
+        </p>
+      </div>
+      <motion.div
+        variants={cardHoverVariants}
+        whileHover="hover"
+        className='flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full'
+      >
+        {transaction.type === 'expense' ? (
+          <BiTrendingDown className='h-5 w-5 text-red-500' />
+        ) : (
+          <BiTrendingUp className='h-5 w-5 text-green-500' />
+        )}
+      </motion.div>
+    </motion.div>
+  );
+};
 
-export default TransactionCard
+export default TransactionCard;
--- src/components/Transactions/subcomps/TransactionsPropsContainer.jsx
+++ src/components/Transactions/subcomps/TransactionsPropsContainer.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const TransactionsPropsContainer = ({handleAlphabetically, handleOrderByMount}) => {
+const TransactionsPropsContainer = ({ handleAlphabetically, handleOrderByMount }) => {
   return (
-    <div className='flex gap-80 m-3 items-center justify-center'>
-          <span className='font-medium w-[100px] cursor-pointer' onClick={handleAlphabetically} >
-            Name
-          </span>
-          <span className='font-medium cursor-pointer' onClick={handleOrderByMount} >
-            Amount
-          </span>
-        </div>  
-  )
-}
+    <div className='flex items-center justify-between px-6 py-3 mx-16 my-2 bg-white border border-gray-200 rounded-lg'>
+      <div
+        className='text-xl font-medium text-gray-700 cursor-pointer hover:text-boldPink'
+        onClick={handleAlphabetically}
+      >
+        Name
+      </div>
+      <div
+        className='text-xl font-medium text-gray-700 cursor-pointer hover:text-boldPink'
+        onClick={handleOrderByMount}
+      >
+        Amount
+      </div>
+    </div>
+  );
+};
 
-export default TransactionsPropsContainer
+export default TransactionsPropsContainer;
