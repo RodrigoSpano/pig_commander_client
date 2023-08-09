632e78e17bd72c62198956b113f428eb7514fec7
Lucas Correa <correalucas1905@gmail.com>
2023-08-09 15:44:17 -0300

    feature(buttom/premium):logic of bottom MP

--- src/components/Profile/ProfileComponent.jsx
+++ src/components/Profile/ProfileComponent.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from "react";
+import { becomePremiumHandler } from "./becomePremiumHandler";
+import { useSelector } from "react-redux";
 
-export default function ProfileComponent () {
-    return (
-        <div>
-            <button>Become Premium</button>
-        </div>
-    )
+export default function ProfileComponent() {
+  //* Variable auxiliar, se la paso a la funcion por params
+  let token;
+
+  //* Selecciono el estado global de usuario
+  const user = useSelector((state) => state.user);
+
+  // * Condicional para buscar el token
+  if (typeof localStorage !== "undefined") {
+    token = localStorage.getItem("token");
+  }
+
+  // * Condicional para que renderize segun si es premium o no
+  return (
+    <div>
+      {/* El que le de estilos y su usario no tiene premium y no tiene configurado el back de mercado pago,
+       puede negar el user.premium y va a ver el PRO en pantalla, sacar este comentario y el ! de negacion al terminar */}
+      {user.premium ? (
+        <p>PRO</p>
+      ) : (
+        <button onClick={() => becomePremiumHandler(token)}>
+          Become Premium
+        </button>
+      )}
+    </div>
+  );
 }
