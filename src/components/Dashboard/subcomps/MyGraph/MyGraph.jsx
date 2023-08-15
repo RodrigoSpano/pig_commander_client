5de02d8a7943b6965d6e000077d5f506eb3f0531
Matias Cancina <cancinamatias@gmail.com>
2023-08-15 11:31:32 -0300

    feat(client/myGraph): agrega grafico de dona

--- src/components/Dashboard/subcomps/MyGraph/MyGraph.jsx
+++ src/components/Dashboard/subcomps/MyGraph/MyGraph.jsx
@@ -1,71 +1,92 @@
-import React from "react";
-import { Card, DonutChart } from "@tremor/react";
-import { motion } from "framer-motion";
-
-const valueFormatter = (number) => `$ ${Intl.NumberFormat("us").format(number).toString()}`;
+import ReactApexChart from "react-apexcharts";
+import { useSelector } from "react-redux";
 
 const DonutChartComponent = () => {
-  const chartData = [
-    { name: "Groceries", sales: 250, color: "#5A67D8" },
-    { name: "Dining Out", sales: 120, color: "#7F9CF5" },
-    { name: "Entertainment", sales: 80, color: "#93C5FD" },
-    { name: "Shopping", sales: 180, color: "#A5E4FF" },
-    { name: "Bills", sales: 300, color: "#D1EEFF" },
-  ];
+  const lastTransactions = useSelector(
+    (state) => state.monthTransactions.transactions
+  );
+  const categories = useSelector((state) => state.others.categories);
 
-  const colors = chartData.map((item) => item.color);
+  const lastExpenses = lastTransactions.filter((t) => t.type === "expense");
 
-  const listItemVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { delay: 0.2, duration: 0.5 } },
+  const categoryExpenses = categories.map((category) => ({
+    id: category.id,
+    name: category.name.replace(/^\w/, (c) => c.toUpperCase()),
+    totalExpense: lastExpenses
+      .filter((transaction) => transaction.category_id === category.id)
+      .reduce((total, transaction) => total + transaction.amount, 0),
+  }));
+
+  const assignColorToCategory = (categoryId) => {
+    const categoryColors = [
+      "#5A67D8",
+      "#7F9CF5",
+      "#93C5FD",
+      "#A5E4FF",
+      "#D1EEFF",
+    ];
+    return categoryColors[
+      categoryExpenses.findIndex((category) => category.id === categoryId) %
+        categoryColors.length
+    ];
+  };
+
+  const chartData = categoryExpenses.map((categoryExpense) => ({
+    x: categoryExpense.name,
+    y: categoryExpense.totalExpense,
+    color: assignColorToCategory(categoryExpense.id),
+  }));
+
+  const options = {
+    chart: {
+      type: "donut",
+    },
+    labels: chartData.map((data) => data.x),
+    colors: chartData.map((data) => data.color),
+    plotOptions: {
+      pie: {
+        donut: {
+          labels: {
+            show: true,
+            total: {
+              show: true,
+              showAlways: true,
+              fontSize: 30,
+              fontWeight: "bold",
+              color: "#000000",
+            },
+          },
+        },
+      },
+    },
+  };
+
+  const series = chartData.map((data) => data.y);
+
+  options.legend = {
+    show: true,
+    position: "bottom", // Puedes ajustar la posición según tu preferencia
+    horizontalAlign: "center",
+    fontSize: "20px", // Ajusta el tamaño de fuente aquí
+    labels: {
+      colors: "#7E7E7E", // Color de las etiquetas
+    },
   };
 
   return (
-    <Card className="max-w-xl max-h-xl p-4 shadow-lg rounded-lg bg-white select-none">
-      <h2 className="text-xl font-bold mb-8 text-boldPink">Spending This Month</h2>
+    <div className="max-w-xl max-h-xl p-4 shadow-lg rounded-lg bg-white select-none">
+      <h2 className="text-xl font-bold mb-8 text-boldPink">
+        Spending This Month
+      </h2>
       <div className="flex justify-center">
-        <DonutChart
-          data={chartData}
-          category="sales"
-          index="name"
-          valueFormatter={valueFormatter}
-          colors={colors}
-          width={400}
-          height={400}
+        <ReactApexChart
+          options={options}
+          series={series}
+          type="donut"
+          width={535}
         />
       </div>
-
-      <hr className="mt-4" />
-
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={{
-          visible: { transition: { staggerChildren: 0.2 } },
-        }}
-      >
-        {chartData.map((item, index) => (
-          <motion.div
-            key={index}
-            variants={listItemVariants}
-            className="flex justify-between items-center py-3"
-          >
-            <div className="flex items-center">
-              <motion.div
-                className="w-4 h-4 rounded-full mr-2"
-                style={{ backgroundColor: item.color }}
-                whileHover={{ scale: 1.2 }}
-              ></motion.div>
-              <span className="text-gray-800">{item.name}</span>
-            </div>
-
-            <div className="text-gray-700">
-              <span className="">${item.sales}</span>
-            </div>
-          </motion.div>
-        ))}
-      </motion.div>
-    </Card>
+    </div>
   );
 };
 
