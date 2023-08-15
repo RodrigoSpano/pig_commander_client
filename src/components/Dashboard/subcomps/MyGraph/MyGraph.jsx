64759904c99b56b1cbbfda3f8a68287060349078
Matias Cancina <cancinamatias@gmail.com>
2023-08-15 17:05:26 -0300

    feat(client/mygraph): grafico de dona

--- src/components/Dashboard/subcomps/MyGraph/MyGraph.jsx
+++ src/components/Dashboard/subcomps/MyGraph/MyGraph.jsx
@@ -1,162 +1,3 @@
-// import React from "react";
-// import { useSelector } from "react-redux";
-// import { PieChart, Pie, Cell, Legend, Tooltip } from "recharts";
-
-// const DonutChartComponent = () => {
-//   const lastTransactions = useSelector(
-//     (state) => state.monthTransactions.transactions
-//   );
-//   const categories = useSelector((state) => state.others.categories);
-
-//   const lastExpenses = lastTransactions.filter((t) => t.type === "expense");
-
-//   const categoryExpenses = categories.map((category) => ({
-//     id: category.id,
-//     name: category.name.replace(/^\w/, (c) => c.toUpperCase()),
-//     totalExpense: lastExpenses
-//       .filter((transaction) => transaction.category_id === category.id)
-//       .reduce((total, transaction) => total + transaction.amount, 0),
-//   }));
-
-//   const assignColorToCategory = (categoryId) => {
-//     const categoryColors = [
-//       "#5A67D8",
-//       "#7F9CF5",
-//       "#93C5FD",
-//       "#A5E4FF",
-//       "#D1EEFF",
-//     ];
-//     return categoryColors[
-//       categoryExpenses.findIndex((category) => category.id === categoryId) %
-//         categoryColors.length
-//     ];
-//   };
-
-//   const chartData = categoryExpenses.map((categoryExpense) => ({
-//     name: categoryExpense.name,
-//     value: categoryExpense.totalExpense,
-//     color: assignColorToCategory(categoryExpense.id),
-//   }));
-
-//   const COLORS = chartData.map((data) => data.color);
-
-//   return (
-//     <div className="max-w-xl max-h-xl p-4 shadow-lg rounded-lg bg-white select-none">
-//       <h2 className="text-xl font-bold mb-8 text-boldPink">
-//         Spending This Month
-//       </h2>
-//       <div className="flex justify-center">
-//         <PieChart width={535} height={500}>
-//           <Pie
-//             data={chartData}
-//             dataKey="value"
-//             nameKey="name"
-//             cx="50%"
-//             cy="50%"
-//             outerRadius={150}
-//             fill="#8884d8"
-//             labelLine={false}
-//           >
-//             {chartData.map((entry, index) => (
-//               <Cell key={`cell-${index}`} fill={COLORS[index]} />
-//             ))}
-//           </Pie>
-//           <Legend iconSize={10}/>
-//           <Tooltip />
-//         </PieChart>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default DonutChartComponent;
-
-// import React from "react";
-// import { useSelector } from "react-redux";
-// import { PieChart, Pie, Cell, Legend, Tooltip } from "recharts";
-
-// const DonutChartComponent = () => {
-//   const lastTransactions = useSelector(
-//     (state) => state.monthTransactions.transactions
-//   );
-//   const categories = useSelector((state) => state.others.categories);
-
-//   const lastExpenses = lastTransactions.filter((t) => t.type === "expense");
-
-//   const categoryExpenses = categories.map((category) => ({
-//     id: category.id,
-//     name: category.name.replace(/^\w/, (c) => c.toUpperCase()),
-//     totalExpense: lastExpenses
-//       .filter((transaction) => transaction.category_id === category.id)
-//       .reduce((total, transaction) => total + transaction.amount, 0),
-//   }));
-
-//   const assignColorToCategory = (categoryId) => {
-//     const categoryColors = [
-//       "#5A67D8",
-//       "#7F9CF5",
-//       "#93C5FD",
-//       "#A5E4FF",
-//       "#D1EEFF",
-//     ];
-//     return categoryColors[
-//       categoryExpenses.findIndex((category) => category.id === categoryId) %
-//         categoryColors.length
-//     ];
-//   };
-
-//   const chartData = categoryExpenses.map((categoryExpense) => ({
-//     name: categoryExpense.name,
-//     value: categoryExpense.totalExpense,
-//     color: assignColorToCategory(categoryExpense.id),
-//   }));
-
-//   const COLORS = chartData.map((data) => data.color);
-
-//   const totalExpenses = chartData.reduce((total, category) => total + category.value, 0);
-
-//   const formattedTotalExpenses = totalExpenses.toLocaleString("en-US", {
-//     style: "currency",
-//     currency: "USD",
-//   });
-
-//   const formattedTotalExpensesWithoutDecimals = formattedTotalExpenses.split(".")[0]; // Remove decimals
-
-//   return (
-//     <div className="flex flex-col items-center justify-center max-w-xl max-h-xl p-4 shadow-lg rounded-lg bg-white select-none">
-//       <h2 className="text-xl font-bold mb-8 text-boldPink">
-//         Spending This Month
-//       </h2>
-//       <div className="text-center mb-4">
-//         <p className="text-lg font-semibold">Total Expenses</p>
-//         <p className="text-2xl font-bold">{formattedTotalExpensesWithoutDecimals}</p>
-//       </div>
-//       <div className="flex justify-center">
-//         <PieChart width={500} height={425}>
-//           <Pie
-//             data={chartData}
-//             dataKey="value"
-//             nameKey="name"
-//             cx="50%"
-//             cy="50%"
-//             outerRadius={150}
-//             fill="#8884d8"
-//             labelLine={false}
-//           >
-//             {chartData.map((entry, index) => (
-//               <Cell key={`cell-${index}`} fill={COLORS[index]} />
-//             ))}
-//           </Pie>
-//           <Legend iconSize={10} />
-//           <Tooltip />
-//         </PieChart>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default DonutChartComponent;
-
 import React from "react";
 import { useSelector } from "react-redux";
 import { Doughnut } from "react-chartjs-2";
@@ -182,11 +23,11 @@ const DonutChartComponent = () => {
 
   const assignColorToCategory = (categoryId) => {
     const categoryColors = [
-      "red",
-      "#7F9CF5",
-      "green",
-      "orange",
-      "black",
+      "#cdb4db",
+      "#ff758f",
+      "#ffafcc",
+      "#bde0fe",
+      "#5aa9e6",
     ];
     return categoryColors[
       categoryExpenses.findIndex((category) => category.id === categoryId) %
@@ -233,11 +74,11 @@ const DonutChartComponent = () => {
   return (
     <div className="flex flex-col items-center justify-center max-w-xl max-h-xl p-4 shadow-lg rounded-lg bg-white select-none">
       <h2 className="text-xl font-bold mb-8 text-boldPink">
-        Gastos de Este Mes
+        Spent This Month
       </h2>
       <div className="text-center mb-4">
-        <p className="text-lg font-semibold">Gastos Totales</p>
-        <p className="text-2xl font-bold">
+        <p className="text-lg font-semibold text-[#374151]">Total Expenses</p>
+        <p className="text-2xl font-bold text-[#374151]">
           {formattedTotalExpensesWithoutDecimals}
         </p>
       </div>
@@ -256,11 +97,10 @@ const DonutChartComponent = () => {
         {chartData.map((data) => (
           <div
             key={data.name}
-            className="flex items-center mb-2 mr-4"
-            style={{ color: data.color }}
+            className={`flex items-center mb-2 mr-4 color[${data.color}]`}
           >
             <div
-              className="w-4 h-4 rounded-full mr-1"
+              className={`w-4 h-4 rounded-full mr-1`} 
               style={{ backgroundColor: data.color }}
             />
             <span>{data.name}</span>
