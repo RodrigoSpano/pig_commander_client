ddd76292af9fbf647e2ad1a19aed1046df71021a
Matias Cancina <cancinamatias@gmail.com>
2023-08-15 13:33:29 -0300

    feat(client/mygraph): grafico de dona

--- src/components/Dashboard/subcomps/MyGraph/MyGraph.jsx
+++ src/components/Dashboard/subcomps/MyGraph/MyGraph.jsx
@@ -1,71 +1,273 @@
-import React from "react";
-import { Card, DonutChart } from "@tremor/react";
-import { motion } from "framer-motion";
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import { PieChart, Pie, Cell, Legend, Tooltip } from "recharts";
 
-const valueFormatter = (number) => `$ ${Intl.NumberFormat("us").format(number).toString()}`;
+// const DonutChartComponent = () => {
+//   const lastTransactions = useSelector(
+//     (state) => state.monthTransactions.transactions
+//   );
+//   const categories = useSelector((state) => state.others.categories);
+
+//   const lastExpenses = lastTransactions.filter((t) => t.type === "expense");
+
+//   const categoryExpenses = categories.map((category) => ({
+//     id: category.id,
+//     name: category.name.replace(/^\w/, (c) => c.toUpperCase()),
+//     totalExpense: lastExpenses
+//       .filter((transaction) => transaction.category_id === category.id)
+//       .reduce((total, transaction) => total + transaction.amount, 0),
+//   }));
+
+//   const assignColorToCategory = (categoryId) => {
+//     const categoryColors = [
+//       "#5A67D8",
+//       "#7F9CF5",
+//       "#93C5FD",
+//       "#A5E4FF",
+//       "#D1EEFF",
+//     ];
+//     return categoryColors[
+//       categoryExpenses.findIndex((category) => category.id === categoryId) %
+//         categoryColors.length
+//     ];
+//   };
+
+//   const chartData = categoryExpenses.map((categoryExpense) => ({
+//     name: categoryExpense.name,
+//     value: categoryExpense.totalExpense,
+//     color: assignColorToCategory(categoryExpense.id),
+//   }));
+
+//   const COLORS = chartData.map((data) => data.color);
+
+//   return (
+//     <div className="max-w-xl max-h-xl p-4 shadow-lg rounded-lg bg-white select-none">
+//       <h2 className="text-xl font-bold mb-8 text-boldPink">
+//         Spending This Month
+//       </h2>
+//       <div className="flex justify-center">
+//         <PieChart width={535} height={500}>
+//           <Pie
+//             data={chartData}
+//             dataKey="value"
+//             nameKey="name"
+//             cx="50%"
+//             cy="50%"
+//             outerRadius={150}
+//             fill="#8884d8"
+//             labelLine={false}
+//           >
+//             {chartData.map((entry, index) => (
+//               <Cell key={`cell-${index}`} fill={COLORS[index]} />
+//             ))}
+//           </Pie>
+//           <Legend iconSize={10}/>
+//           <Tooltip />
+//         </PieChart>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default DonutChartComponent;
+
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import { PieChart, Pie, Cell, Legend, Tooltip } from "recharts";
+
+// const DonutChartComponent = () => {
+//   const lastTransactions = useSelector(
+//     (state) => state.monthTransactions.transactions
+//   );
+//   const categories = useSelector((state) => state.others.categories);
+
+//   const lastExpenses = lastTransactions.filter((t) => t.type === "expense");
+
+//   const categoryExpenses = categories.map((category) => ({
+//     id: category.id,
+//     name: category.name.replace(/^\w/, (c) => c.toUpperCase()),
+//     totalExpense: lastExpenses
+//       .filter((transaction) => transaction.category_id === category.id)
+//       .reduce((total, transaction) => total + transaction.amount, 0),
+//   }));
+
+//   const assignColorToCategory = (categoryId) => {
+//     const categoryColors = [
+//       "#5A67D8",
+//       "#7F9CF5",
+//       "#93C5FD",
+//       "#A5E4FF",
+//       "#D1EEFF",
+//     ];
+//     return categoryColors[
+//       categoryExpenses.findIndex((category) => category.id === categoryId) %
+//         categoryColors.length
+//     ];
+//   };
+
+//   const chartData = categoryExpenses.map((categoryExpense) => ({
+//     name: categoryExpense.name,
+//     value: categoryExpense.totalExpense,
+//     color: assignColorToCategory(categoryExpense.id),
+//   }));
+
+//   const COLORS = chartData.map((data) => data.color);
+
+//   const totalExpenses = chartData.reduce((total, category) => total + category.value, 0);
+
+//   const formattedTotalExpenses = totalExpenses.toLocaleString("en-US", {
+//     style: "currency",
+//     currency: "USD",
+//   });
+
+//   const formattedTotalExpensesWithoutDecimals = formattedTotalExpenses.split(".")[0]; // Remove decimals
+
+//   return (
+//     <div className="flex flex-col items-center justify-center max-w-xl max-h-xl p-4 shadow-lg rounded-lg bg-white select-none">
+//       <h2 className="text-xl font-bold mb-8 text-boldPink">
+//         Spending This Month
+//       </h2>
+//       <div className="text-center mb-4">
+//         <p className="text-lg font-semibold">Total Expenses</p>
+//         <p className="text-2xl font-bold">{formattedTotalExpensesWithoutDecimals}</p>
+//       </div>
+//       <div className="flex justify-center">
+//         <PieChart width={500} height={425}>
+//           <Pie
+//             data={chartData}
+//             dataKey="value"
+//             nameKey="name"
+//             cx="50%"
+//             cy="50%"
+//             outerRadius={150}
+//             fill="#8884d8"
+//             labelLine={false}
+//           >
+//             {chartData.map((entry, index) => (
+//               <Cell key={`cell-${index}`} fill={COLORS[index]} />
+//             ))}
+//           </Pie>
+//           <Legend iconSize={10} />
+//           <Tooltip />
+//         </PieChart>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default DonutChartComponent;
+
+import React from "react";
+import { useSelector } from "react-redux";
+import { Doughnut } from "react-chartjs-2";
+import { Chart, ArcElement } from "chart.js";
 
 const DonutChartComponent = () => {
-  const chartData = [
-    { name: "Groceries", sales: 250, color: "#5A67D8" },
-    { name: "Dining Out", sales: 120, color: "#7F9CF5" },
-    { name: "Entertainment", sales: 80, color: "#93C5FD" },
-    { name: "Shopping", sales: 180, color: "#A5E4FF" },
-    { name: "Bills", sales: 300, color: "#D1EEFF" },
+  const lastTransactions = useSelector(
+    (state) => state.monthTransactions.transactions
+  );
+  const categories = useSelector((state) => state.others.categories);
+
+  Chart.register(ArcElement);
+
+  const lastExpenses = lastTransactions.filter((t) => t.type === "expense");
+
+  const categoryExpenses = categories.map((category) => ({
+    id: category.id,
+    name: category.name.replace(/^\w/, (c) => c.toUpperCase()),
+    totalExpense: lastExpenses
+      .filter((transaction) => transaction.category_id === category.id)
+      .reduce((total, transaction) => total + transaction.amount, 0),
+  }));
+
+  const assignColorToCategory = (categoryId) => {
+    const categoryColors = [
+      "red",
+      "#7F9CF5",
+      "green",
+      "orange",
+      "black",
+    ];
+    return categoryColors[
+      categoryExpenses.findIndex((category) => category.id === categoryId) %
+        categoryColors.length
+    ];
+  };
+
+  const chartData = categoryExpenses.map((categoryExpense) => ({
+    name: categoryExpense.name,
+    value: categoryExpense.totalExpense,
+    color: assignColorToCategory(categoryExpense.id),
+  }));
+
+  const COLORS = chartData.map((data) => data.color);
+
+  const totalExpenses = chartData.reduce(
+    (total, category) => total + category.value,
+    0
+  );
+
+  const formattedTotalExpenses = totalExpenses.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  const formattedTotalExpensesWithoutDecimals =
+    formattedTotalExpenses.split(".")[0];
+
+  const chartDataSets = [
+    {
+      data: chartData.map((data) => data.value),
+      backgroundColor: COLORS,
+    },
   ];
 
-  const colors = chartData.map((item) => item.color);
-
-  const listItemVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { delay: 0.2, duration: 0.5 } },
+  const chartOptions = {
+    legend: {
+      display: true,
+      position: "bottom",
+    },
+    cutoutPercentage: 70,
   };
 
   return (
-    <Card className="max-w-xl max-h-xl p-4 shadow-lg rounded-lg bg-white select-none">
-      <h2 className="text-xl font-bold mb-8 text-boldPink">Spending This Month</h2>
-      <div className="flex justify-center">
-        <DonutChart
-          data={chartData}
-          category="sales"
-          index="name"
-          valueFormatter={valueFormatter}
-          colors={colors}
-          width={400}
-          height={400}
-        />
+    <div className="flex flex-col items-center justify-center max-w-xl max-h-xl p-4 shadow-lg rounded-lg bg-white select-none">
+      <h2 className="text-xl font-bold mb-8 text-boldPink">
+        Gastos de Este Mes
+      </h2>
+      <div className="text-center mb-4">
+        <p className="text-lg font-semibold">Gastos Totales</p>
+        <p className="text-2xl font-bold">
+          {formattedTotalExpensesWithoutDecimals}
+        </p>
       </div>
-
-      <hr className="mt-4" />
-
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={{
-          visible: { transition: { staggerChildren: 0.2 } },
-        }}
-      >
-        {chartData.map((item, index) => (
-          <motion.div
-            key={index}
-            variants={listItemVariants}
-            className="flex justify-between items-center py-3"
+      <div className="flex justify-center">
+        <div >
+          <Doughnut
+            data={{
+              datasets: chartDataSets,
+              labels: chartData.map((data) => data.name),
+            }}
+            options={chartOptions}
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap justify-center mt-14">
+        {chartData.map((data) => (
+          <div
+            key={data.name}
+            className="flex items-center mb-2 mr-4"
+            style={{ color: data.color }}
           >
-            <div className="flex items-center">
-              <motion.div
-                className="w-4 h-4 rounded-full mr-2"
-                style={{ backgroundColor: item.color }}
-                whileHover={{ scale: 1.2 }}
-              ></motion.div>
-              <span className="text-gray-800">{item.name}</span>
-            </div>
-
-            <div className="text-gray-700">
-              <span className="">${item.sales}</span>
-            </div>
-          </motion.div>
+            <div
+              className="w-4 h-4 rounded-full mr-1"
+              style={{ backgroundColor: data.color }}
+            />
+            <span>{data.name}</span>
+          </div>
         ))}
-      </motion.div>
-    </Card>
+      </div>
+    </div>
   );
 };
 
