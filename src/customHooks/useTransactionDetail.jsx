9614ba7ca6651c910c511f6a28e68ab1359f9524
Victoria Rodríguez <vickyrodriguez544@gmail.com>
2023-08-15 18:39:34 +0200

    feat(client/wallet/transactions):delete

--- src/customHooks/useTransactionDetail.jsx
+++ src/customHooks/useTransactionDetail.jsx
@@ -1,18 +1,20 @@
-import { useSelector } from "react-redux";
+import { deleteExpense, deleteIncome } from "@/redux/actions/transactionsActions";
+import { useCookies } from "react-cookie";
+import { useDispatch, useSelector } from "react-redux";
 import Swal from "sweetalert2";
 
 const useTransactionDetail = () => {
   const categories = useSelector((state) => state.others.categories);
   const methods = useSelector((state) => state.others.methods);
+  const dispatch = useDispatch();
+  const [cookies] = useCookies();
 
   const handleDetail = (transaction) => {
     const findCategory = categories.find(
       (cat) => cat.id === transaction.category_id
     );
 
-    const findMethod = methods.find(
-      (met) => met.id === transaction.method_id
-    );
+    const findMethod = methods.find((met) => met.id === transaction.method_id);
 
     Swal.fire({
       position: "center",
@@ -25,13 +27,42 @@ const useTransactionDetail = () => {
       <p>Amount: $${transaction.amount}</p>
       <p>Date: ${transaction.createdAt.substring(0, 10)}</p>
       `,
-      showConfirmButton: false,
+      showconfirmButton: true,
       showCloseButton: true,
+      confirmButtonText: "Delete",
+      confirmButtonColor: "#F11000",
       customClass: {
         title: "text-2xl font-semibold bg-blue-200 p-2",
         closeButton: "text-red-500 hover:text-red-100",
       },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        if(transaction.type === 'expense'){
+          const { token } = cookies; 
+          const data = {
+            ...transaction,
+            token,
+          };
+          dispatch(
+            deleteExpense(data)
+          );
+           } [transaction, cookies];
+
+      if (result.isConfirmed) {
+          if (transaction.type === 'income'){
+          const { token } = cookies; 
+          const data = {
+            ...transaction,
+            token,
+          };
+          dispatch(
+            deleteIncome(data)
+          );
+           } [transaction, cookies];
+       }
+      }
     });
+   
   };
 
   return { handleDetail };
