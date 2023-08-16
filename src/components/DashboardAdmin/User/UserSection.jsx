5674cbd1a2d5cb2459a41d6570398738fe16a28a
lihuel <zeregalihuel@gmail.com>
2023-08-16 20:56:34 -0300

    Feature(Client/AdminDashboar): Animaciones

--- src/components/DashboardAdmin/User/UserSection.jsx
+++ src/components/DashboardAdmin/User/UserSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import UserImage from "../../../utils/Images/userimageexample.png";
 
@@ -10,18 +11,44 @@ const user = {
 };
 
 const UserSection = () => {
+  const userSectionVariants = {
+    initial: { x: -20, opacity: 0 },
+    animate: { x: 0, opacity: 1, transition: { duration: 0.6, delay: 0.4 } },
+  };
+
   return (
-    <div className="flex items-center">
+    <motion.div
+      className="flex items-center"
+      variants={userSectionVariants}
+      initial="initial"
+      animate="animate"
+    >
       <div className="flex flex-col mr-4">
-        <h2 className="text-xl font-semibold">
+        <motion.h2
+          className="text-xl font-semibold"
+          initial={{ y: -10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { duration: 0.6, delay: 0.6 } }}
+        >
           {user.name} {user.lastname}
-        </h2>
-        <p className="text-gray-500">{user.occupation}</p>
+        </motion.h2>
+        <motion.p
+          className="text-gray-500"
+          initial={{ y: -10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1, transition: { duration: 0.8, delay: 0.7 } }}
+        >
+          {user.occupation}
+        </motion.p>
       </div>
       <div className="w-16 h-16 overflow-hidden rounded-full">
-        <Image src={user.image} width={90} height={90} alt="UserImage" />
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1, transition: { duration: 0.6, delay: 0.8 } }}
+          className="w-16 h-16 overflow-hidden rounded-full"
+        >
+          <Image src={user.image} width={90} height={90} alt="UserImage" />
+        </motion.div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
