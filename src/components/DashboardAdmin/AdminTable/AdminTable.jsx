6900d4a346e300e9f0a385cc3668206763c1d496
lihuel <zeregalihuel@gmail.com>
2023-08-16 14:24:14 -0300

    Feature(Client/AdminDashboard): Implementacion de tabla y espaciados

---
+++ src/components/DashboardAdmin/AdminTable/AdminTable.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip, getKeyValue} from "@nextui-org/react";
+import {DeleteIcon} from "./DeleteIcon";
+import {EyeIcon} from "./EyeIcon";
+import {columns, users} from "./data";
+
+const statusColorMap = {
+  active: "success",
+  paused: "danger",
+  vacation: "warning",
+};
+
+export default function App() {
+  const renderCell = React.useCallback((user, columnKey) => {
+    const cellValue = user[columnKey];
+
+    switch (columnKey) {
+      case "name":
+        return (
+          <User
+            avatarProps={{radius: "lg", src: user.avatar}}
+            description={user.email}
+            name={cellValue}
+          >
+            {user.email}
+          </User>
+        );
+      case "role":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-sm capitalize">{cellValue}</p>
+            <p className="text-bold text-sm capitalize text-default-400">{user.team}</p>
+          </div>
+        );
+      case "status":
+        return (
+          <Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
+            {cellValue}
+          </Chip>
+        );
+      case "actions":
+        return (
+          <div className="relative flex items-center gap-2">
+            <Tooltip content="Details">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EyeIcon />
+              </span>
+            </Tooltip>
+            <Tooltip color="danger" content="Delete user">
+              <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                <DeleteIcon />
+              </span>
+            </Tooltip>
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
+
+  return (
+  <Table aria-label="Example table with custom cells">
+      <TableHeader columns={columns}>
+        {(column) => (
+          <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
+            {column.name}
+          </TableColumn>
+        )}
+      </TableHeader>
+      <TableBody items={users}>
+        {(item) => (
+          <TableRow key={item.id}>
+            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
--- src/components/DashboardAdmin/DashboardAdminComponent.jsx
+++ src/components/DashboardAdmin/DashboardAdminComponent.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import AdminTable from './AdminTable/AdminTable'
 
 const DashboardAdminComponent = () => {
   return (
-    <div>
-      <div className="flex justify-between items-center my-4 mx-12">
+    <div className="mx-12 ">
+
+      <section className="flex justify-between items-center my-12">
         <div>Administrator Dashboard</div>
         <div className="flex items-center">
           <div className="mx-2">AdminName</div>
           <div className="mx-2">photo</div>
         </div>
-      </div>
-      <div className="flex justify-between m-12">
+      </section>
+
+      <section className="flex justify-between my-12">
         <div className="w-1/4 p-4 bg-blue-300 flex justify-center items-center">Total User</div>
         <div className="w-1/4 p-4 bg-green-300 flex justify-center items-center">Basic User</div>
         <div className="w-1/4 p-4 bg-yellow-300 flex justify-center items-center">Pro User</div>
         <div className="w-1/4 p-4 bg-red-300 flex justify-center items-center">Banned User</div>
-      </div>
+      </section>
+
+      <section>
+        <AdminTable />
+      </section>
     </div>
   );
 };
