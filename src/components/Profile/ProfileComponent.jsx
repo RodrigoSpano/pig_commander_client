20a3dd23e62264a36c01e43e531abc2fbbbdd49f
lihuel <zeregalihuel@gmail.com>
2023-08-17 12:41:53 -0300

    Fix(Client/Dashboard): StatsContainer desktop responsive

--- src/components/Profile/ProfileComponent.jsx
+++ src/components/Profile/ProfileComponent.jsx
@@ -1,24 +1,80 @@
-"use client";
 import React from "react";
-import { becomePremiumHandler } from "./becomePremiumHandler";
-import { useSelector } from "react-redux";
-import { useCookies } from "react-cookie";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
+import { becomePremiumHandler } from './becomePremiumHandler';
+import { useCookies } from 'react-cookie';
 
-export default function ProfileComponent({ user }) {
+function asdf({ user }) {
   const [cookies, setCookie] = useCookies();
 
-  // * Condicional para que renderize segun si es premium o no
   return (
-    <div>
-      {/* El que le de estilos y su usario no tiene premium y no tiene configurado el back de mercado pago,
-       puede negar el user.premium y va a ver el PRO en pantalla, sacar este comentario y el ! de negacion al terminar */}
+    <div className="mb-4">
       {user?.premium ? (
-        <p>PRO</p>
+        <p className="text-lg font-semibold text-regularPink">PRO</p>
       ) : (
-        <button onClick={() => becomePremiumHandler(cookies.token)}>
+        <button
+          onClick={() => becomePremiumHandler(cookies.token)}
+          className="bg-regularPink text-white py-2 px-6 rounded-lg"
+        >
           Become Premium
         </button>
       )}
     </div>
   );
+}
+
+function ProfileComponent({ user }) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [cookies, setCookie] = useCookies();
+
+  return (
+    <div>
+      {user?.premium ? (
+        <p className="text-lg font-semibold text-regularPink">PRO</p>
+      ) : (
+        <Button onPress={onOpen} className="bg-gradient-to-r from-pink-400 to-pink-600 text-white py-2 rounded-md hover:from-pink-500 hover:to-pink-700 transition-all duration-300">Become Premium</Button>
+      )}
+
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Why become premium?</ModalHeader>
+              <ModalBody>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam pulvinar risus non risus hendrerit venenatis.
+                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Nullam pulvinar risus non risus hendrerit venenatis.
+                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                </p>
+                <p>
+                  Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit
+                  dolor adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis.
+                  Velit duis sit officia eiusmod Lorem aliqua enim laboris do dolor eiusmod.
+                  Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur
+                  proident Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button className="text-regularPink bg-transparent" onClick={onClose}>
+                  Close
+                </Button>
+                <Button className="bg-gradient-to-r from-pink-400 to-pink-600 text-white py-2 rounded-md hover:from-pink-500 hover:to-pink-700 transition-all duration-300" onClick={() => becomePremiumHandler(cookies.token)}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+}
+
+module.exports = {
+  asdf,
+  ProfileComponent,
 }
