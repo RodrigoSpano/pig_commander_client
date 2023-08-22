4a2ed91537dd89d37206f862e877f5f2b9b2f6f2
Matias Cancina <cancinamatias@gmail.com>
2023-08-22 15:00:23 -0300

    FEAT(client/tarnsactionscomponent): arregla detalle en ingles

--- src/customHooks/useTransactionDetail.jsx
+++ src/customHooks/useTransactionDetail.jsx
@@ -87,7 +87,84 @@ const useTransactionDetail = () => {
     });
   };
 
-  return {  handleDetailEs };
+  const handleDetail = (transaction) => {
+    const findCategory = categories.find(
+      (cat) => cat.id === transaction.category_id
+    );
+
+    const findMethod = methods.find((met) => met.id === transaction.method_id);
+
+    Swal.fire({
+      position: "center",
+      title: `
+      <p>Categoría: ${findCategory.name.replace(/^\w/, (c) =>
+        c.toUpperCase()
+      )}</p>
+      <p>Method: ${findMethod.name.replace(/^\w/, (c) => c.toUpperCase())}</p>
+      <p>Name: ${transaction.name}</p>
+      <p>Amount: $${transaction.amount}</p>
+      <p>Date: ${transaction.createdAt.substring(0, 10)}</p>
+      `,
+      showconfirmButton: true,
+      showCloseButton: true,
+      confirmButtonText: "Delete",
+      confirmButtonColor: "#F11000",
+      customClass: {
+        title: "text-2xl font-semibold bg-blue-200 p-2",
+        closeButton: "text-red-500 hover:text-red-100",
+      },
+    }).then((result) => {
+      
+      if (result.isConfirmed) {
+        if(transaction.type === 'expense'){
+          const { token } = cookies; 
+          const data = {
+            id: transaction.id,
+            token,
+          };
+          dispatch(
+            deleteExpenseMonth(data)
+          );
+           } [transaction, cookies];
+        if(transaction.type === 'income'){
+          const { token } = cookies; 
+          const data = {
+            id: transaction.id,
+            token,
+          };
+          dispatch(
+            deleteIncomeMonth(data)
+          );
+            } [transaction, cookies];
+
+        if(transaction.type === 'expense'){
+          const { token } = cookies; 
+          const data = {
+            ...transaction,
+            token,
+          };
+          dispatch(
+            deleteExpense(data)
+          );
+           } [transaction, cookies];
+
+      if (result.isConfirmed) {
+          if (transaction.type === 'income'){
+          const { token } = cookies; 
+          const data = {
+            ...transaction,
+            token,
+          };
+          dispatch(
+            deleteIncome(data)
+          );
+           } [transaction, cookies];
+       }
+      }
+    });
+  };
+
+  return { handleDetail, handleDetailEs };
 };
 
 export default useTransactionDetail;
