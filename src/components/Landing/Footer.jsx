d626149d6b1ad8cf29ac6b2a5cad2262a2ce6f14
Lucas Correa <correalucas1905@gmail.com>
2023-08-23 15:39:53 -0300

    fix(footer/links): arreglue links del footer y navbar

--- src/components/Landing/Footer.jsx
+++ src/components/Landing/Footer.jsx
@@ -9,12 +9,12 @@ const Footer = () => {
     <footer className='rounded-t-md shadow-black bg-white dark:bg-mediumGrayDarkMode select-none'>
       <div className='flex flex-col xl:items-start lg:items-start md:flex-row justify-around items-center md:px-12 py-8'>
         {/* CONTACT */}
-        <div className='flex flex-col items-center md:items-start mb-4 md:mb-0'>
+        <div className='flex flex-col items-center  mb-4 md:mb-0'>
           <Image src={Logo} width={50} height={60} alt='Logo' />
-          <div className='flex flex-col md:flex-row md:items-center mt-2'>
-            <div className='flex items-center'>
+          <div className='flex flex-col items-baseline mt-2'>
+            <div className='flex items-center mb-1'>
               <HiMail className='text-boldPink text-xl mr-1' />
-              <p>email.com</p>
+              <p>pigcommandersp@gmail.com</p>
             </div>
             <div className='flex items-center mt-2 md:mt-0 md:ml-4'>
               <HiPhone className='text-boldPink text-xl mr-1' />
@@ -57,30 +57,18 @@ const Footer = () => {
         {/* PRODUCT */}
         <div className='flex flex-col items-center md:items-start mb-4 md:mb-0'>
           <h3 className='pb-2 text-3xl font-bold dark:text-mediumPinkDark'>Product</h3>
-          <Link href='#' className='pb-1 text-base'>
-            Take Tour
-          </Link>
-          <Link href='#' className='pb-1 text-base'>
-            Live Chat
-          </Link>
-          <Link href='#' className='pb-1 text-base'>
+          <Link href='/reviews' className='pb-1 text-base'>
             Reviews
           </Link>
         </div>
 
-        {/* NEWSLETTER */}
-        <div className='flex flex-col items-center md:items-start mb-4 md:mb-0'>
-          <h3 className='pb-2 text-3xl font-bold dark:text-mediumPinkDark'>Newsletter</h3>
-          <Link href='#' className='pb-1 text-base'>
-            Stay Up to Date
-          </Link>
-        </div>
+      
       </div>
 
       <hr className='my-6 md:my-8'></hr>
 
       <div className='text-center md:text-center px-6 py-6 md:px-12'>
-        Copyright 2023 PigCommander.Com all rights reserved
+        Copyright 2023 | PigCommander.com | All rights reserved
       </div>
     </footer>
   );
