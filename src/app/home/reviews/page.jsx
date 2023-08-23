7d1642d74cfd8300b0c2b177148cf3b51be5b0a7
Matias Cancina <cancinamatias@gmail.com>
2023-08-23 13:52:03 -0300

    feat(client/reviews): traduccion

--- src/app/home/reviews/page.jsx
+++ src/app/home/reviews/page.jsx
@@ -2,6 +2,7 @@
 import ReviewPage from "@/components/Landing/Reviews/ReviewPage";
 import ReviewPageEs from "@/components/LandingEs/Reviews/ReviewPageEs";
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Reviews() {
   const selectedLanguage = useSelector((state) => state.language);
